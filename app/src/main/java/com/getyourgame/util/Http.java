043cf46882b33b243d74a2f8533fe1f7bf7abd42
gurmantovani <gurmantovani@gmaill.com>
2016-01-28 14:12:21 -0200

    Correções no Layout da lista de oportunidades e transações

--- app/src/main/java/com/getyourgame/util/Http.java
+++ app/src/main/java/com/getyourgame/util/Http.java
@@ -37,8 +37,8 @@ public class Http extends AsyncTask<String, Void, Object> {
     protected Object doInBackground(String... params) {
         try {
             //String url = "http://10.0.2.2/getYourGameWS/getyourgame/"+ws.getServico();
-            String url = "http://192.168.25.38/getYourGameWS/getyourgame/"+ws.getServico();
-            //String url = "http://192.168.1.112/getYourGameWS/getyourgame/"+ws.getServico();
+            //String url = "http://192.168.25.38/getYourGameWS/getyourgame/"+ws.getServico();
+            String url = "http://192.168.1.112/getYourGameWS/getyourgame/"+ws.getServico();
 
             SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
             simpleClientHttpRequestFactory.setReadTimeout(5000);
